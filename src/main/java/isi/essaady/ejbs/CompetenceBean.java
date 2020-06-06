9ea940855dafc7cf27ee7c7891063abaa214b0da
Kuohamkun <50623506+Kuohamkun@users.noreply.github.com>
2020-06-06 23:29:54 +0100

    [F/dashComps] Create CRUD operations for the Competence ejb

--- src/main/java/isi/essaady/ejbs/CompetenceBean.java
+++ src/main/java/isi/essaady/ejbs/CompetenceBean.java
@@ -22,12 +22,51 @@ public class CompetenceBean {
 	
     public CompetenceBean() {}
     
+    
     /**
-     * getAllComps() method
+     * Selects all availble competences.
+     * 
+     * @return List<Competence>	Compentces list
      */
     @SuppressWarnings("unchecked")
 	public List<Competence> getAllComps() {
     	return em.createNamedQuery("Competence.findAll").getResultList();
     }
+    
+    
+    /**
+     * Updates an existing competence.
+     * 
+     * @param comp	The new updated competence object.
+     * @return Competence	The edited competence object
+     */
+	public Competence updateComp(Competence comp) {
+    	return em.merge(comp);
+    }
+	
+	
+	/**
+     * Creates a new competence.
+     * 
+     * @param comp	The new competence object.
+     */
+	public void createComp(Competence comp) {
+		em.persist(comp);
+	}
+	
+	
+	/**
+     * Removes a competence.
+     * 
+     * @param comp	The competence object to remove.
+     */
+	public void deleteComp(Competence comp) {
+		if (!em.contains(comp)) {
+			comp = em.merge(comp);
+		}
+		em.remove(comp);
+	}
+	
+	
 
 }
