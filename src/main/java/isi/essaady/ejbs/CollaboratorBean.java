830aae9e4d24a75827c4f862f122cc938990ee51
Kuohamkun <50623506+Kuohamkun@users.noreply.github.com>
2020-06-10 20:16:39 +0100

    [F/dashCollabs] Create CRUD operations for the Collaborator ejb

--- src/main/java/isi/essaady/ejbs/CollaboratorBean.java
+++ src/main/java/isi/essaady/ejbs/CollaboratorBean.java
@@ -22,12 +22,49 @@ public class CollaboratorBean {
 	
     public CollaboratorBean() {}
     
+    
     /**
-     * getAllCollabs() method
+     * Gets all availble collaborators.
+     * 
+     * @return List<Collaborator>  All collaborators
      */
     @SuppressWarnings("unchecked")
 	public List<Collaborator> getAllCollabs() {
     	return em.createNamedQuery("Collaborator.findAll").getResultList();
     }
+    
+    
+    /**
+     * Updates an existing Collaborator.
+     * 
+     * @param  collab  The new updated Collaborator object.
+     * @return  Collaborator  The edited Collaborator object
+     */
+	public Collaborator updateCollab(Collaborator collab) {
+    	return em.merge(collab);
+    }
+	
+	
+	/**
+     * Creates a new collaborator.
+     * 
+     * @param comp	The new collaborator object.
+     */
+	public void createCollab(Collaborator collab) {
+		em.persist(collab);
+	}
+	
+	
+	/**
+     * Removes a collaborator.
+     * 
+     * @param collab  The collaborator object to remove.
+     */
+	public void deleteCollab(Collaborator collab) {
+		if (!em.contains(collab)) {
+			collab = em.merge(collab);
+		}
+		em.remove(collab);
+	}
 
 }
