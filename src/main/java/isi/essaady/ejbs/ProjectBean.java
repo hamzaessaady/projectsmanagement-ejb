1624b2838148b4f60dfb6773078e678f6f36a360
Kuohamkun <50623506+Kuohamkun@users.noreply.github.com>
2020-06-12 17:57:05 +0100

    [F/projSummary] Create CRUD operations for the Project ejb

--- src/main/java/isi/essaady/ejbs/ProjectBean.java
+++ src/main/java/isi/essaady/ejbs/ProjectBean.java
@@ -29,5 +29,39 @@ public class ProjectBean {
 	public List<Project> getAllProjects() {
     	return em.createNamedQuery("Project.findAll").getResultList();
     }
+    
+    
+    /**
+     * Updates an existing Project.
+     * 
+     * @param proj	The new updated project object
+     * @return Project	The edited project object
+     */
+	public Project updateProj(Project proj) {
+    	return em.merge(proj);
+    }
+	
+	
+	/**
+     * Creates a new project.
+     * 
+     * @param proj	The new project object.
+     */
+	public void createProj(Project proj) {
+		em.persist(proj);
+	}
+	
+	
+	/**
+     * Removes a project.
+     * 
+     * @param proj  The project object to remove.
+     */
+	public void deleteProj(Project proj) {
+		if (!em.contains(proj)) {
+			proj = em.merge(proj);
+		}
+		em.remove(proj);
+	}
 
 }
